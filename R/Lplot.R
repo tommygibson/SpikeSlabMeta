3a417f72870a69953ae80737b3aef5ee6c1aea2c
Tommy Gibson <tommy.a.gibson@gmail.com>
2021-06-04 14:54:34 -0700

    Update Lplot

--- R/Lplot.R
+++ R/Lplot.R
@@ -7,7 +7,9 @@ library(here)
 library(tidyverse)
 library(ggplot2)
 library(extrafont)
-font_import(pattern = "lmroman*")
+library(xtable)
+
+# font_import(pattern = "lmroman*")
 
 source("R/spike.functions.R")
 
@@ -26,9 +28,10 @@ L <- c(1, 10, 100, 1000)
 LRp <- list()
 for(i in 1:2) LRp[[i]] <- matrix(nrow = 10000, ncol = length(L))
 
-set.seed(1515)
 
 for(j in 1:length(sigma.d)){
+  
+  set.seed(1516)
   
   delta.i <- rnorm(S, mean = delta0, sd = sigma.d[j])
   beta.i <- rnorm(S, mean = beta0, sd = sigma.b[j])
@@ -87,43 +90,37 @@ LRp.lowhigh <- cbind.data.frame(rbind(LRp.low, LRp.high),
                                 as.factor(rep(c("SDs = 0.5", "SDs = 1"), each = dim(LRp.low)[1])))
 names(LRp.lowhigh) <- c("LR+", "L", "SDs")
 
-LRp.lowhigh %>%
-  ggplot(mapping = aes(x = `LR+`, linetype = L, size = L)) +
-    theme_bw() +
-    theme(text = element_text(size = 12, family = "LM Roman 10")) +
-    geom_density(fill = "black", alpha = 0.05) +
-    facet_wrap( ~ SDs) +
-    xlim(c(0.5, 12)) +
-    scale_linetype_manual(values = c("dotted", "dotdash", "solid", "dashed")) +
-    scale_size_manual(values = c(0.5, 0.5, 0.5, 0.5)) + 
-    labs(title = "Posterior distribution of LR+ for varying L", ylab = "")
+Lplot <- LRp.lowhigh %>%
+           ggplot(mapping = aes(x = `LR+`, linetype = L, size = L)) +
+             theme_bw() +
+             theme(text = element_text(size = 12, family = "LM Roman 10")) +
+             geom_density(fill = "black", alpha = 0.05) +
+             facet_wrap( ~ SDs) +
+             xlim(c(0, 10)) +
+             scale_linetype_manual(values = c("dotted", "dotdash", "solid", "dashed")) +
+             scale_size_manual(values = c(0.5, 0.5, 0.5, 0.5)) + 
+             labs(ylab = c("", ""))
 
-ggplot(data = LRp.low, mapping = aes(x = `LR+`, linetype = L, size = L)) +
-  geom_density(fill = "black", alpha = 0.05) +
-  xlim(c(0.5, 15)) +
-  scale_linetype_manual(values = c(1, 2, 3, 4)) +
-  scale_size_manual(values = c(0.5, 0.5, 1, 0.5)) + 
-  theme_bw() +
-  labs(title = "Posterior distribution of LR+ for varying L", ylab = "")
-ggplot(data = LRp.high, mapping = aes(x = `LR+`, linetype = L, size = L)) +
-  geom_density(fill = "black", alpha = 0.05) +
-  xlim(c(0.5, 15)) +
-  scale_linetype_manual(values = c(1, 2, 3, 4)) +
-  scale_size_manual(values = c(0.5, 0.5, 1, 0.5)) + 
-  theme_bw() +
-  labs(title = "Posterior distribution of LR+ for varying L", ylab = "")
 
-apply(LRp[[1]], 2, mean)
-apply(LRp[[1]], 2, var)
-apply(LRp[[1]], 2, quantile, c(.025, .975))
-apply(LRp[[2]], 2, mean)
-apply(LRp[[2]], 2, var)
-apply(LRp[[2]], 2, quantile, c(.025, .975))
-
-ggsave("TeX/L_plot.pdf", plot = last_plot(), device = "pdf",
+ggsave("TeX/L_plot.pdf", plot = Lplot, device = "pdf",
        width = 5, height = 5, units = "in")
 
+simsum <- function(x){
+  sd <- apply(x, 2, sd)
+  length <- as.vector(diff(apply(x, 2, quantile, c(.025, .975))))
+  
+  return(sigfig(cbind(sd, length), n = 3))
+}
 
+sd0.5 <- simsum(LRp[[1]])
+sd1 <- simsum(LRp[[2]])
+
+Lplot.tab <- cbind.data.frame(sd0.5, sd1)
+rownames(Lplot.tab) <- c("L = 1", "L = 10", "L = 100", "L = 1000")
+
+print(xtable(Lplot.tab, caption = "Posterior distribution of LR+ for varying L for two simulated datasets",
+             type = "latex"),
+      file = "TeX/Lplot.tex")
 
 
                             
