827f6b7dea0b00f5fd9a9ed9e1d0c507748247b6
Tommy Gibson <tommy.a.gibson@gmail.com>
2022-06-29 18:04:36 -0700

    Cleaned code for simulation results
    Removed old commented code

--- R/cts.summary.R
+++ R/cts.summary.R
@@ -1,25 +1,14 @@
 ##### Summary of CTS simulation
 
-library(knitr)
 library(tidyverse)
-library(xtable)
 library(extrafont)
-#library(gridExtra)
 library(here)
 library(ggplot2)
 library(ggpattern)
 library(scales)
 library(here)
-#library(simhelpers)
 
-# cts.5 <- readRDS(here("R", "Results", "cts.sim.5.rds"))[[1]]
-# cts.10 <- readRDS(here("R", "Results", "cts.sim.10.rds"))[[1]] 
-# cts.30 <- readRDS(here("R", "Results", "cts.sim.30.rds"))[[1]] 
-# cts.50 <- readRDS(here("R", "Results", "cts.sim.50.rds"))[[1]]
-# cts.S.N.2 <- readRDS(here("R", "Results", "cts.sim.S.N.2.rds"))[[1]]
-# cts.S.N.3 <- readRDS(here("R", "Results", "cts.sim.S.N.3.rds"))[[1]]
-
-# cts.5 <- readRDS(here("R", "Results", "cts.sim.5.postprocess.rds"))[[1]] 
+# read in simulation results and combine to one data.frame
 cts.10 <- readRDS(here("R", "Results", "cts.sim.10.postprocess.rds"))[[1]]
 cts.30 <- readRDS(here("R", "Results", "cts.sim.30.postprocess.rds"))[[1]]
 cts.50 <- readRDS(here("R", "Results", "cts.sim.50.postprocess.rds"))[[1]]
@@ -36,57 +25,10 @@ cts.all <- bind_rows(cts.10, cts.30, cts.50) %>%
             pattern = c("LRm", "LRp"),
             replace = c("LR-", "LR+"))
 
+# remove original datasets to save memory
+rm(cts.10, cts.30, cts.50)
 
-# cts.names <- c("LRm", "LRp", "NPV", "PPV", "Sens", "Spec")
-
-# cts <- list()
-# cts.S.N <- list()
-# 
-# for(i in 1:length(cts.10)){
-# 
-#   cts[[i]] <- cts.5[[i]] %>% 
-#     drop_na() %>% 
-#     type_convert() %>%
-#     mutate(CTS = cts.names[i]) %>%
-#     relocate(CTS) %>%
-#     rbind(cts.10[[i]] %>% 
-#             drop_na() %>% 
-#             type_convert() %>%
-#             mutate(CTS = cts.names[i]) %>%
-#             relocate(CTS)) %>%
-#     rbind(cts.30[[i]] %>% 
-#             drop_na() %>% 
-#             type_convert() %>%
-#             mutate(CTS = cts.names[i]) %>%
-#             relocate(CTS)) %>%
-#     rbind(cts.50[[i]] %>% 
-#             drop_na() %>% 
-#             type_convert() %>%
-#             mutate(CTS = cts.names[i]) %>%
-#             relocate(CTS)) %>%
-#     left_join(targets, by = c("CTS", "sigma.delta")) %>%
-#     type_convert(guess_integer = TRUE)
-#   
-#   # cts.S.N[[i]] <- cts.S.N.2[[i]] %>% 
-#   #   drop_na() %>% 
-#   #   type_convert() %>%
-#   #   mutate(CTS = cts.names[i]) %>%
-#   #   relocate(CTS) %>%
-#   #   rbind(cts.S.N.3[[i]] %>% 
-#   #           drop_na() %>% 
-#   #           type_convert() %>%
-#   #           mutate(CTS = cts.names[i]) %>%
-#   #           relocate(CTS))
-#   
-# }
-
-rm(cts.5, cts.10, cts.30, cts.50)
-
-# names(cts) <- names(cts.S.N) <- cts.names
-# 
-# all.cts <- do.call(rbind, cts)
-# all.cts.S.N <- do.call(rbind, cts.S.N)
-
+## create summary statistics to be plotted
 full.summary <- cts.all %>%
   group_by(CTS, S, sigma.delta, Method) %>%
   mutate(m = mean(mean.est),
@@ -115,41 +57,15 @@ full.summary <- cts.all %>%
             upper.Length = `95% Length` + 1.96 * mcse.Length
     ) 
 
+# function to specify x-axis plot breaks for bias plot
 breaks_fun <- function(x){
   if(max(abs(x) > 0.1)){
     seq(-0.005, 0.005, 0.005)
   }
   else seq(-0.1, 0.1, 0.1)
 }
-# full.summary.S.N <- all.cts.S.N %>%
-#   group_by(CTS, S, sigma.delta, Method) %>%
-#   summarize(bias = mean(mean.est - target.mean),
-#             rmse = sqrt(mean((mean.est - target.mean)^2)),
-#             mcse = sqrt(v / 2500),
-#             avg.sd = mean(sd.est),
-#             cover = mean(ci.lower < target.mean & ci.upper > target.mean),
-#             length = mean(ci.upper - ci.lower),
-#             min = min(mean.est),
-#             max = max(mean.est)) 
 
-# yint.dummy <- data.frame(stat = c("Bias", "95% Coverage", "95% Length", "RMSE"), 
-#                          Y = c(0, .95, NA, NA))
-# 
-# (LRp.plot <- full.summary %>%
-#   filter(CTS == "LRp") %>%
-#   pivot_longer(cols = c("Bias", "RMSE", "95% Coverage", "95% Length"), names_to = "stat") %>%
-#   ggplot(aes(x = as.factor(S), y = value, group = interaction(Method, as.factor(sigma.delta)), 
-#                color = as.factor(sigma.delta), linetype = Method)) +
-#     geom_point() +
-#     geom_line() + 
-#     geom_hline(data = yint.dummy, aes(yintercept = Y), alpha = 0.5, linetype = 2) +
-#     facet_wrap(~stat, scales = "free") +
-#     theme_bw() +
-#     labs(x = "No. studies",
-#          y = NULL,
-#          color = expression(sigma[delta])) +
-#     scale_color_manual(values = c("deepskyblue2", "orchid3", "firebrick2")))
-  
+# bias plot
 
 (bias.plot <- full.summary %>%
   ggplot(aes(x = as.factor(S), y = Bias, #group = interaction(Method, factor(sigma.delta)), 
@@ -174,20 +90,7 @@ breaks_fun <- function(x){
 
 
 
-
-# (bias.plot <- full.summary %>%
-#     ggplot(aes(x = as.factor(S), y = Bias, group = interaction(Method, factor(sigma.delta)), 
-#                color = factor(sigma.delta), linetype = Method)) +
-#     geom_point(position = position_dodge(width = 0.2)) +
-#     geom_line(position = position_dodge(width = 0.2)) +
-#     geom_hline(yintercept = 0) +
-#     geom_linerange(aes(ymin = lower.Bias, ymax = upper.Bias), position = position_dodge(width = 0.2), linetype = 1) + 
-#     facet_wrap(~CTS, nrow = 2, ncol = 3,
-#                scales = "free") +
-#     labs(x = "Number of studies", y = "Bias") +
-#     theme_bw() +
-#     theme(text = element_text(size = 12, family = "LM Roman 10")) +
-#     scale_color_manual(values = RColorBrewer::brewer.pal(3, "Set2"), name = expression(sigma[delta])))
+# 95% interval coverage plot
 
 (cover.plot <- full.summary %>%
     ggplot(aes(x = factor(S), y = `95% Coverage`, group = interaction(Method, factor(sigma.delta)),
@@ -208,6 +111,8 @@ breaks_fun <- function(x){
                           labels = c(expression(CTS[0]), expression(CTS[h]))) +
     scale_color_manual(values = RColorBrewer::brewer.pal(3, "Set2"), name = expression(sigma[delta])))
   
+# root-mean squared error plot
+
 (rmse.plot <- full.summary %>%
     ggplot(aes(x = as.factor(S), y = RMSE, group = interaction(Method, factor(sigma.delta)),
                linetype = Method, color = as.factor(sigma.delta))) +
@@ -223,6 +128,8 @@ breaks_fun <- function(x){
                           labels = c(expression(CTS[0]), expression(CTS[h]))) +
     theme(text = element_text(size = 12, family = "LM Roman 10")) +
     scale_color_manual(values = RColorBrewer::brewer.pal(3, "Set2"), name = expression(sigma[delta])) )
+
+# 95% interval length plot
 
 (length.plot <- full.summary %>%
     ggplot(aes(x = as.factor(S), y = `95% Length`, group = interaction(Method, factor(sigma.delta)),
@@ -242,8 +149,10 @@ breaks_fun <- function(x){
     theme(text = element_text(size = 12, family = "LM Roman 10")) +
     scale_color_manual(values = RColorBrewer::brewer.pal(3, "Set2"), name = expression(sigma[delta])))
 
-ggsave("TeX/bias_plot.pdf", plot = bias.plot,
-       device = "pdf", width = 6.5, height = 7, units = "in", dpi = 600)
+# save plots
+# bias plot had to be saved manually because legend is messed up with ggsave
+# problem has to do with ggpattern, not sure what the issue is
+
 ggsave("TeX/cover_plot.pdf", plot = cover.plot, 
        device = "pdf", width = 6, height = 6, units = "in", dpi = 600)
 ggsave("TeX/rmse_plot.pdf", plot = rmse.plot, 
