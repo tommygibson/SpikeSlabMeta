95d11ee428dc947279d0a3ac8f82942311195aed
Tommy Gibson <tommy.a.gibson@gmail.com>
2022-09-06 15:44:04 -0700

    Consolidating code for summarizing spike/slab simulation

--- R/spike.summary.R
+++ R/spike.summary.R
@@ -3,11 +3,11 @@
 library(tidyverse)
 library(here)
 library(xtable)
+library(extrafont)
 
 spike <- readRDS(here("R", "Results", "meta.spike.results.rds"))[[1]]
 
 (spike.table <- spike %>% 
-  filter(delta0 != 0.5) %>%
   group_by(delta0, sigma.delta) %>%
   summarize(Mean = mean(spike),
             SD = sd(spike)) %>%
@@ -21,10 +21,22 @@ print(xtable(spike.table, caption = "Spike table", type = "latex", digits = 4),
       file = "TeX/spike.table.tex", include.rownames = FALSE)
   
   
-  
-(spike.box <- spike %>%
-    filter(delta0 != 0.5) %>%
-  ggplot(aes(x = factor(delta0), y = spike)) +
-  geom_boxplot() +
-  facet_wrap(~ sigma.delta, nrow = 1))
+spike$sigma.delta <- factor(spike$sigma.delta,
+                            levels = c(0.1, 0.25, 0.5),
+                            labels = c("sigma[delta] == 0.1", 
+                                       "sigma[delta] == 0.25",
+                                       "sigma[delta] == 0.5"))
 
+spike.boxplot <- spike %>%
+  ggplot(mapping = aes(x = as.factor(delta0), y = spike)) +
+  theme_bw() +
+  theme(text = element_text(size = 12, family = "LM Roman 10")) +
+  geom_boxplot(outlier.size = 0, outlier.shape = 1, alpha = 1) +
+  labs(x = TeX("$\\delta_0$"), y = TeX("P($\\delta_0 = 0$ | $Y_k$)")) +
+  #labs(x = expression(delta[0]), y = expression(paste('P(', delta[0], '=0 | ', Y[k], ')'))) +
+  ylim(c(0, 1)) +
+  facet_wrap(~sigma.delta,
+             labeller = label_parsed)
+
+# ggsave("TeX", "spike_boxplot.pdf", spike.boxplot,
+#        device = "pdf", height = 4, width = 5, units = "in", dpi = 600)
